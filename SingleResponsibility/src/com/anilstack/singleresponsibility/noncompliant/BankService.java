d0a8eafd1e7206496372c82f9383ffbc8103d248
anilstack <aniltech068@gmail.com>
2023-05-01 17:06:34 +0530

    added code based on previous review comments

---
+++ SingleResponsibility/src/com/anilstack/singleresponsibility/noncompliant/BankService.java
@@ -0,0 +1,18 @@
+package com.anilstack.singleresponsibility.noncompliant;
+
+
+/**
+ * @author 𝓐𝓷𝓲𝓵𝓡𝓪𝓳𝓾
+ */
+public class BankService {
+
+    public long deposit (long amount, String accountNumber) {
+        //logic
+        return 0;
+    }
+
+    public long withDraw(long amountToWithDraw, String accountNumber) {
+        //logic
+        return 0;
+    }
+}
---
+++ SingleResponsibility/src/com/anilstack/singleresponsibility/noncompliant/LoanService.java
@@ -0,0 +1,12 @@
+package com.anilstack.singleresponsibility.noncompliant;
+
+
+/**
+ * @author 𝓐𝓷𝓲𝓵𝓡𝓪𝓳𝓾
+ */
+public class LoanService {
+
+    public void findLoanInterestInformation(String loanType) {
+        //logic
+    }
+}
